996f9acf6ed34eb124a1f4d289f019ece83c4382
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-24 18:37:05 +0900

    ImageField. upload_to setting

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from .models import Post
 from django.shortcuts import render
 
 def post(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
     return HttpResponse('post-list')
